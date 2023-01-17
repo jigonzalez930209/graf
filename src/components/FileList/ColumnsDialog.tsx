0729e3effcf56504e1e79135ede0fe63cf747a74
José Iván González Jorge <56707603+jigonzalez930209@users.noreply.github.com>
2023-01-17 20:08:22 -0300

    feat: Add columns drag and drop selectos

--- src/components/FileList/ColumnsDialog.tsx
+++ src/components/FileList/ColumnsDialog.tsx
@@ -4,82 +4,12 @@ import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
+import { useSnackbar } from 'notistack';
+import _ from 'lodash';
+
 import { GrafContext } from '../../context/GraftContext';
-import { Box } from '@mui/system';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Grid } from '@mui/material';
 
-const options = [
-  'xaxis',
-  'yaxis',
-  'yaxis2',
-  'null',
-];
-
-const ITEM_HEIGHT = 48;
-
-type LongMenuProp = {
-  onSelect: ({ value, id }: { value: "xaxis" | "yaxis" | "yaxis2" | "null", id: number }) => void
-  id: number
-  selectedAxis: "xaxis" | "yaxis" | "yaxis2" | null
-}
-
-
-export const LongMenu = ({ onSelect, id, selectedAxis }: LongMenuProp) => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleSelect = (value: "xaxis" | "yaxis" | "yaxis2" | 'null') => {
-    setAnchorEl(null);
-    onSelect({ value: value === 'null' ? null : value, id })
-  }
-
-  return (
-    <div>
-      <IconButton
-        aria-label="more"
-        id="long-button"
-        aria-controls={open ? 'long-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
-        aria-haspopup="true"
-        onClick={handleClick}
-        sx={{ transform: 'scale(0.5)' }}
-      >
-        <MoreVertIcon />
-      </IconButton>
-      <Menu
-        id="long-menu"
-        MenuListProps={{
-          'aria-labelledby': 'long-button',
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          style: {
-            maxHeight: ITEM_HEIGHT * 4.5,
-            width: '20ch',
-          },
-        }}
-      >
-        {options.map((option) => (
-          <MenuItem key={option} selected={option === (selectedAxis === null ? 'null' : selectedAxis)} onClick={() => handleSelect(option as "xaxis" | "yaxis" | "yaxis2" | 'null')}>
-            {option}
-          </MenuItem>
-        ))}
-      </Menu>
-    </div>
-  );
-}
+import DragDrop, { ColumnsGroup } from './drag-drop/DragDrop';
 
 type ColumnsDialogProp = {
   open: boolean,
@@ -88,31 +18,74 @@ type ColumnsDialogProp = {
 
 const ColumnsDialog = ({ open, setOpen }: ColumnsDialogProp) => {
   const { graftState: { columns }, setSelectedColumns } = React.useContext(GrafContext)
+  const { enqueueSnackbar } = useSnackbar()
 
-  console.log(columns)
+  const [currentColumns, setCurrentColumns] = React.useState<ColumnsGroup>(null)
 
-  const handleChangeColumnAxis = ({ id, value }: { id: number, value: "xaxis" | "yaxis" | "yaxis2" }) => {
-    console.log(id, value)
-
-    setSelectedColumns({
-      ...columns,
-      columns: columns.columns.map(
-        column =>
-        ({
-          ...column,
-          axis: column.index === id ? value : column.axis,
-          selected: column.index === id ? true : column.selected
-        }))
-    })
+  const onDragChange = (columns: ColumnsGroup) => {
+    setCurrentColumns(columns)
   }
 
   const handleClose = () => {
     setOpen(false);
+    columns && setSelectedColumns(columns)
   };
 
   const handleApply = () => {
+
+    if (_.isEmpty(currentColumns.xAxis) && _.isEmpty(currentColumns.yAxis) && _.isEmpty(currentColumns.y2Axis)) {
+      console.warn('No columns selected');
+      enqueueSnackbar('No columns selected', { variant: 'error' })
+      return;
+    }
+
+    if (currentColumns.xAxis.length !== currentColumns.yAxis.length) {
+      console.warn('X and Y axis must have the same number of columns');
+      enqueueSnackbar('X and Y axis must have the same number of columns', { variant: 'error' })
+      return;
+    }
+
+    if (currentColumns.xAxis.length < currentColumns.y2Axis.length) {
+      console.warn('Y2 should be have maximum the number of column of x Axis');
+      enqueueSnackbar('Y2 should be have maximum the number of column of x Axis', { variant: 'error' })
+      return;
+    }
+
+    setSelectedColumns({
+      ...columns,
+      columns: columns.columns.map((column, index) => {
+        const axis = currentColumns.xAxis.includes(column.name) && 'xaxis' || currentColumns.yAxis.includes(column.name) && 'yaxis' || currentColumns.y2Axis.includes(column.name) && 'yaxis2' || null
+        const selected = axis !== null ? true : column.selected
+        let axisGroup
+        if (axis === null) {
+          axisGroup = null
+        } else {
+          axisGroup = () => {
+            switch (axis) {
+              case 'xaxis':
+                return _.findIndex(currentColumns.xAxis, (name) => name === column.name)
+              case 'yaxis':
+                return _.findIndex(currentColumns.yAxis, (name) => name === column.name)
+              case 'yaxis2':
+                return _.findIndex(currentColumns.y2Axis, (name) => name === column.name)
+              default:
+                return null
+            }
+          }
+        }
+
+        console.log({ axisGroup, name: column.name, axis })
+        return {
+          ...column,
+          axis,
+          selected,
+          axisGroup: axisGroup && axisGroup(),
+        }
+      }),
+
+    })
+
     setOpen(false);
-    setSelectedColumns(columns)
   }
 
   return (
@@ -124,19 +97,12 @@ const ColumnsDialog = ({ open, setOpen }: ColumnsDialogProp) => {
       aria-describedby="alert-dialog-description"
     >
       <DialogTitle id="alert-dialog-title">
-        Select a axis to each column of the file or leave it blank
+        Drag and Drop columns in axis
       </DialogTitle>
-      <DialogContent>
-        <Grid container spacing={2} columns={6}>
-          {columns.columns.map((column, index) => (
-            <Grid xs={2} item key={`${column.name}-${index}`} sx={{ display: 'flex', justifyContent: 'start' }}>
-              <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                {column.name}
-              </Box>
-              <LongMenu onSelect={handleChangeColumnAxis} id={column.index} selectedAxis={column.axis} />
-            </Grid>
-          ))}
-        </Grid>
+      <DialogContent sx={{ width: '100hv', height: '80hv', margin: 0, padding: 0 }}>
+
+        <DragDrop onChange={onDragChange} />
+
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Cancel</Button>
