910e5982e1c4805b804dbc87c0555c8968aa0e56
José Iván González Jorge <56707603+jigonzalez930209@users.noreply.github.com>
2023-01-31 16:17:27 -0300

    feat: Add radios and refactor table info graft

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -5,13 +5,30 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import { Radio, Box } from '@mui/material';
+import { ProcessFile } from '../../interfaces/interfaces';
 
 type TableProps = {
   header: string[]
-  data: string[][]
+  file: ProcessFile
+  truncateIn?: number
+  onSelectChange: (row: number) => void
 }
 
-const DenseTable: React.FC<TableProps> = ({ header, data }) => {
+const DenseTable: React.FC<TableProps> = ({ header, file, truncateIn = 50, onSelectChange }) => {
+  const [selectedValue, setSelectedValue] = React.useState(file.selectedInvariableContentIndex);
+  const [currentData, setCurrentData] = React.useState(null)
+
+  const handleSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(parseInt(event.target.value));
+
+    onSelectChange(parseInt(event.target.value) + 1)
+  }
+
+  React.useEffect(() => {
+    setCurrentData(file.invariableContent.slice(0, truncateIn))
+    onSelectChange(selectedValue + 1)
+  }, [truncateIn])
 
   return (
     <TableContainer style={{
@@ -19,21 +36,45 @@ const DenseTable: React.FC<TableProps> = ({ header, data }) => {
       color: 'white'
     }}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
+        {header?.length > 0 && <TableHead>
           <TableRow>
             {header.map((cell, i) =>
               <TableCell key={cell + i} style={{ color: 'wheat' }} align="center">{cell}</TableCell>
             )}
           </TableRow>
-        </TableHead>
+        </TableHead>}
         <TableBody>
-          {data.map((row, i) => (
+          {currentData?.map((row, i) => (
             <TableRow
               key={`row-${i}`}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
+              <TableCell
+                padding='none'
+                size='small'
+                align='left'
+                sx={{
+                  p: 0,
+                  width: 100,
+                }} component="th" scope="row">
+                <Box sx={{ p: 0, alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
+                  {i + 1}
+                  <Radio
+                    checked={selectedValue === i}
+                    onChange={handleSelect}
+                    value={i}
+                    name="radio-buttons"
+                    sx={{
+                      '& .MuiSvgIcon-root': {
+                        fontSize: 15,
+                      },
+                    }}
+                  />
+                </Box>
+
+              </TableCell>
               {row.map((cell, j) =>
-                <TableCell key={`cell-${i}-${j}`} style={{ color: 'wheat' }} component="th" scope="row">
+                <TableCell key={`cell-${i}-${j}`} sx={{ color: them => them.palette.grey[700] }} component="th" scope="row">
                   {cell}
                 </TableCell>
               )}
