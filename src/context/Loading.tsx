49e5dd0493f3dbb8732e58445d9106f301833b5e
José Iván González Jorge <56707603+jigonzalez930209@users.noreply.github.com>
2023-02-03 18:17:55 -0300

    feat: Add loading provider

---
+++ src/context/Loading.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+
+export type LoadingsContextProps = {
+  loading: { loading: boolean };
+  setLoading: (loading: boolean) => void;
+}
+
+export const LoadingsContext = React.createContext<LoadingsContextProps>({} as LoadingsContextProps);
+
+interface props {
+  children: JSX.Element | JSX.Element[]
+  initialState: boolean
+}
+
+export const LoadingProvider = ({ children, initialState }: props) => {
+
+  const [loading, dispatch] = React.useReducer(loadingReducer, { loading: initialState });
+
+  const setLoading = (loading: boolean) => dispatch({ type: 'setLoading', payload: loading })
+
+  React.useEffect(() => {
+    setLoading(initialState)
+  }, [initialState])
+
+  return (
+    <LoadingsContext.Provider value={{
+      loading,
+      setLoading
+    }}>
+      {children}
+    </LoadingsContext.Provider>
+  )
+
+}
+
+type LoadingAction = {
+  type: "setLoading"
+
+  payload: boolean
+
+};
+
+export const loadingReducer = (
+  state: { loading: boolean },
+  action: LoadingAction
+) => {
+  switch (action.type) {
+    case "setLoading":
+      return {
+        loading: action.payload as boolean,
+      };
+
+    default:
+      return state;
+  }
+};
