4179f6396ca29e331bba427f0c4fe999fc25631c
José Iván González Jorge <56707603+jigonzalez930209@users.noreply.github.com>
2023-02-19 09:54:27 -0300

    feat: Add loading animation

--- src/components/Help/components/Text.tsx
+++ src/components/Help/components/Text.tsx
@@ -7,29 +7,45 @@ import remarkToc from 'remark-toc'
 import rehypeHighlight from 'rehype-highlight'
 import rehypeKatex from 'rehype-katex'
 import rehypeRaw from 'rehype-raw'
+import Backdrop from '@mui/material/Backdrop';
+
+import Loader from '../../Loader/Loader'
+
 import './style-md.css'
 
 const MdText = ({ style, mdPath }) => {
   const [text, setText] = React.useState<string>('');
-  const [isDark, setIsDark] = React.useState(true)
+  const [loading, setLoading] = React.useState(true)
 
   React.useEffect(() => {
+    setLoading(true)
     fetch(mdPath)
-      .then((res) => res.text())
-      .then((text) => setText(text));
+      .then((res) => {
+        return res.text()
+      })
+      .then((text) => setText(text)).finally(() => setLoading(false));
   }, [mdPath]);
 
 
   return (
-    <ReactMarkdown
-      className="markdown-body"
-      remarkPlugins={[remarkGfm, remarkSlug, remarkToc]}
-      rehypePlugins={[rehypeRaw, rehypeHighlight, rehypeKatex]}
-      components={{}}
-    // rehypePlugins={[rehypeKatex]}
-    >
-      {text}
-    </ReactMarkdown>
+    <>
+      {loading ? <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.modal + 1 }}
+        open={loading}
+        onClick={() => { }}
+      >
+        <Loader type="hash" />
+      </Backdrop> :
+        <ReactMarkdown
+          className="markdown-body"
+          remarkPlugins={[remarkGfm, remarkSlug, remarkToc]}
+          rehypePlugins={[rehypeRaw, rehypeHighlight, rehypeKatex]}
+          components={{}}
+        >
+          {text}
+        </ReactMarkdown>
+      }
+    </>
   );
 };
 
