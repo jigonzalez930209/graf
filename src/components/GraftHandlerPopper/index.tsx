722f7c2ffc629c76c907bd972c57b40121027c63
José Iván González Jorge <56707603+jigonzalez930209@users.noreply.github.com>
2023-05-03 13:44:17 -0300

    feat: Change width

--- src/components/GraftHandlerPopper/index.tsx
+++ src/components/GraftHandlerPopper/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { FormControl, IconButton, InputLabel, MenuItem, Paper, Popover, Select, Box, Grid, Typography, Slider } from '@mui/material';
+import { FormControl, IconButton, InputLabel, MenuItem, Paper, Popover, Popper, Select, Box, Grid, Typography, Slider } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
 
 import { IGraftImpedanceType, IGrafType } from "../../interfaces/interfaces";
 import { GrafContext } from '../../context/GraftContext';
 import Tooltip from '../Tooltip';
+import { rotate } from '../../utils';
 
 
 const GraftHandlerPopper = () => {
@@ -42,7 +43,10 @@ const GraftHandlerPopper = () => {
           aria-describedby={id}
           size="small"
           color="inherit"
-          sx={{ ml: 2 }}
+          sx={{
+            ml: 2,
+            ...(open && rotate)
+          }}
           onMouseEnter={handleClick}
         >
           <SettingsIcon fontSize='inherit' />
@@ -59,10 +63,16 @@ const GraftHandlerPopper = () => {
           horizontal: 'left',
         }}
       >
-        <Paper sx={{ p: 1, width: 350, height: 200 }}>
+        <Paper
+          onMouseLeave={() => setOpen(false)}
+          sx={{
+            p: 1,
+            width: 350,
+            height: 180
+          }}>
           <Grid container>
-            <Grid xs={6} item>
-              <FormControl variant="filled" sx={{ m: 1, minWidth: 150 }}>
+            <Grid item>
+              <FormControl variant="filled" sx={{ m: 1, minWidth: 250 }}>
                 <InputLabel id="type">Type</InputLabel>
                 <Select
                   labelId="type"
@@ -80,8 +90,8 @@ const GraftHandlerPopper = () => {
               </FormControl>
             </Grid>
             {fileType === 'teq4Z' &&
-              <Grid xs={6} item>
-                <FormControl variant="filled" sx={{ m: 1, minWidth: 150 }}>
+              <Grid item>
+                <FormControl variant="filled" sx={{ m: 1 }}>
                   <InputLabel id="impedance-type">Impedance type</InputLabel>
                   <Select
                     labelId="impedance-type"
@@ -100,9 +110,9 @@ const GraftHandlerPopper = () => {
                 </FormControl>
               </Grid>
             }
-            {fileType === 'teq4' && (<Grid xs={6} item>
-              <FormControl variant="filled" sx={{ m: 1, minWidth: 200, maxHeight: 20 }}>
-                <Box sx={{ maxWidth: 180 }}>
+            {fileType === 'teq4' && (<Grid item>
+              <FormControl variant="filled" sx={{ m: 1, maxHeight: 20 }}>
+                <Box sx={{ maxWidth: 250 }}>
                   <Typography id="input-slider" gutterBottom>
                     Steps between points
                   </Typography>
